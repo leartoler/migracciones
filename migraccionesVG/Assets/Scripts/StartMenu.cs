5321e49e96bf26a53a3cc8ef2f1ecb0e7a16f3a1
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-05-27 14:55:19 -0500

    StartMenu added
    Has been added the starmenu, that contains the eng and esp language.

---
+++ migraccionesVG/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public AudioSource noise1;
+    
+
+    void Start()
+    {
+        AudioSource[] sounds = GetComponents<AudioSource>();// audioañadido en start
+        noise1 = sounds[0];
+    }
+
+    private void Musica()
+    {
+        noise1.Play();
+    }
+
+    public void Esp()
+    {
+        SceneManager.LoadScene("MainMenuEsp");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Se quita");
+        Application.Quit();
+    }
+
+    public void Eng()
+    {
+        SceneManager.LoadScene("MainMenuEng");
+    }
+}
+
